2b347274830e4d42bf1300e30c1de352f591e3ac
Sonu Kumar <sonukumarraj007@gmail.com>
2024-07-25 00:34:39 +0530

    Delete code-snippets directory

--- code-snippets/get-active-observables-from-subject.js
+++
@@ -1,7 +0,0 @@
-getActiveObservablesFromSubject = function(subject) {
-    const observers = subject.observers;
-    for (let index = 0; index < observers.length; index++) {
-        const observer = observers[index];
-        console.log(observer.destination._next);
-    }
-}
